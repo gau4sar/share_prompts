37e3969a268ceaa4895e94f62ebc5eef87239e04
GST <gaurav4sarma@gmail.com>
2023-07-14 18:19:34 +0530

    Loading view is added to login state

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -10,6 +10,7 @@ const Nav = () => {
   const { data: session } = useSession();
 
   const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isSessionLoading, setSessionLoading] = useState(true);
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
@@ -23,15 +24,22 @@ const Nav = () => {
 
   useEffect(() => {
     console.log("session-> " + session);
+    console.log("session-> " + session?.user != null);
 
-    if (session == undefined || session?.user == null) {
-      console.log("session == undefined");
+    if (session == undefined || session == null) {
+      setSessionLoading(true);
+      console.log("session == undefined ");
+      // } else if (session == null) {
+      //   console.log("session?.user == null");
+
       setLoggedIn(false);
       localStorage.setItem(IS_LOGGED_IN, false);
+      setSessionLoading(false);
     } else {
-      console.log("session != undefined");
+      console.log("session else");
       setLoggedIn(true);
       localStorage.setItem(IS_LOGGED_IN, true);
+      setSessionLoading(false);
     }
   }, [session]);
 
@@ -50,41 +58,49 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isLoggedIn ? (
-          <div className="flex gap-3 md:gap-5">
-            <Link href="/create-prompt" className="black_btn">
-              Create Post
-            </Link>
-
-            <button type="button" onClick={signOut} className="outline_btn">
-              Sign Out
-            </button>
-
-            <Link href="/profile">
-              <Image
-                src={session?.user.image}
-                width={37}
-                height={37}
-                className="rounded-full"
-                alt="profile"
-              />
-            </Link>
-          </div>
+        {isSessionLoading ? (
+          <>
+            <div className="small-spinner"></div>
+          </>
         ) : (
           <>
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <button
-                  type="button"
-                  key={provider.name}
-                  onClick={() => {
-                    signIn(provider.id);
-                  }}
-                  className="black_btn"
-                >
-                  Sign in
+            {isLoggedIn ? (
+              <div className="flex gap-3 md:gap-5">
+                <Link href="/create-prompt" className="black_btn">
+                  Create Post
+                </Link>
+
+                <button type="button" onClick={signOut} className="outline_btn">
+                  Sign Out
                 </button>
-              ))}
+
+                <Link href="/profile">
+                  <Image
+                    src={session?.user.image}
+                    width={37}
+                    height={37}
+                    className="rounded-full"
+                    alt="profile"
+                  />
+                </Link>
+              </div>
+            ) : (
+              <>
+                {providers &&
+                  Object.values(providers).map((provider) => (
+                    <button
+                      type="button"
+                      key={provider.name}
+                      onClick={() => {
+                        signIn(provider.id);
+                      }}
+                      className="black_btn"
+                    >
+                      Sign in
+                    </button>
+                  ))}
+              </>
+            )}
           </>
         )}
       </div>
